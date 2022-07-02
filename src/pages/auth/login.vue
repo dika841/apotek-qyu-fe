168a330cdc2210e3e2347677aaf614a44ed60e74
Randika <randikaa1999@gmail.com>
2022-07-02 18:44:31 +0700

    Randika | Add Drop down

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,21 +1,19 @@
 <script setup>
-import  login from "../../assets/illustration/Login.png"
-
+import login from "../../assets/illustration/Login.png";
 </script>
-
 
 <template>
   <section class="h-screen bg-blue-300">
-    <div class=" h-full text-gray-800">
+    <div class="h-full text-gray-800">
       <div
-        class=" flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+        class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
       >
         <div
           class="grow-0 bg-blue-300 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
         >
           <img height="380" :src="login" alt="" />
         </div>
-        <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+        <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0 pr-24">
           <form>
             <div
               class="flex flex-row items-center justify-center lg:justify-start"
@@ -121,26 +119,27 @@ import  login from "../../assets/illustration/Login.png"
                   >Remember me</label
                 >
               </div>
-              <a href="#!" class="text-gray-800">Forgot password?</a>
+              <a href="#!" class="text-gray-800">Lupa Password ?</a>
             </div>
 
             <div class="text-center lg:text-left">
-               <router-link to="/customer">
-              <button
-                type="button"
-                class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-              >
-                Login
-              </button>
+              <router-link to="/customer">
+                <button
+                  type="button"
+                  class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Login
+                </button>
               </router-link>
               <p class="text-sm font-semibold mt-2 pt-1 mb-0">
-                Don't have an account?
+                Belum punya akun ?
                 <router-link to="/register">
-                <a
-                  href="#!"
-                  class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                  >Register</a>
-                  </router-link>
+                  <a
+                    href="#!"
+                    class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                    >Register</a
+                  >
+                </router-link>
               </p>
             </div>
           </form>
